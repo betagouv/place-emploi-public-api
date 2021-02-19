7d836e288a17362a2ce20b69af64e35f0a2e4769
Chaïb Martinez <chaib@zyyne.com>
2021-02-19 18:07:48 +0100

    minor fix

--- routes/geturl.js
+++ routes/geturl.js
@@ -4,53 +4,40 @@ var axios = require('axios');
 var FormData = require('form-data');
 var data = new FormData();
 
+/* GET url offer on place-emploi-public.gouv.fr with a offer id */
+router.get('/', function (req, res, next) {
 
-/* GET url listing. */
-router.get('/', function(req, res, next) {
+    console.log('searching for  req.query.id =' + req.query.id);
+    data.append('keywords', '2021-527984');
 
-console.log('searching for  req.query.id ='+ req.query.id);
-   
+    var config = {
+        method: 'post',
+        url: 'https://place-emploi-public.gouv.fr/wp-admin/admin-ajax.php?action=search_process',
+        headers: {
+            ...data.getHeaders()
+        },
+        data: data
+    };
 
-data.append('keywords', '2021-527984');
+    axios(config)
+        .then(function (response) {
+            //console.log('reponse count='+response.data.count)
+            if (response.data.count) {
+                if (response.data.count == 1) {
+                    //console.log(response.data.offers[0].url);
+                    res.send('https://place-emploi-public.gouv.fr' + response.data.offers[0].url);
+                } else {
+                    res.send('too many offers for this id');
+                }
 
-var config = {
-  method: 'post',
-  url: 'https://place-emploi-public.gouv.fr/wp-admin/admin-ajax.php?action=search_process',
-  headers: { 
-    'Cookie': 'PHPSESSID=8531cf7512b391b47dd70144614f6249', 
-    ...data.getHeaders()
-  },
-  data : data
-};
-
-axios(config)
-.then(function (response) {
-  //var offre=JSON.stringify(response.data)
-  console.log('reponse count='+response.data.count)
-  if(response.data.count){
-      if(response.data.count == 1){
-        console.log(response.data.offers[0].url);
-        res.send('https://place-emploi-public.gouv.fr'+response.data.offers[0].url);
-
-      } else {
-        res.send('too many offers for this id');
-      }
-
-  } else {
-    console.log('no offer :(');
-    res.send('no offer for this id');
-
-  }
-
- 
-})
-.catch(function (error) {
-  console.log(error);
-});
-
-
-
-
+            } else {
+                //console.log('no offer :(');
+                res.send('no offer for this id');
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 });
 
 module.exports = router;
