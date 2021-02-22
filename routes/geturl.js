3adf034ed8cf95e93a3ed1fb9eb6f3b2a7c0360b
Chaïb Martinez <chaib@zyyne.com>
2021-02-22 15:20:55 +0100

    bug fix

--- routes/geturl.js
+++ routes/geturl.js
@@ -2,16 +2,14 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 var FormData = require('form-data');
-var data = new FormData();
 
 /* GET url offer on place-emploi-public.gouv.fr with a offer id */
 router.get('/', function (req, res, next) {
+    var data = new FormData();
 
     //console.log('searching for  req.query.id =' + req.query.id);
-    data.append('reference',req.query.id);
-    data.append('action','get_reference');
-   
-
+    data.append('reference', req.query.id);
+    data.append('action', 'get_reference');
     var config = {
         method: 'post',
         url: 'https://place-emploi-public.gouv.fr/wp-admin/admin-ajax.php',
@@ -20,23 +18,21 @@ router.get('/', function (req, res, next) {
         },
         data: data
     };
-
     axios(config)
         .then(function (response) {
             //console.log('reponse data='+response.data);
             //console.log(response.data);
             //console.log('reponse data.url ='+response.data.url);
-            if (response.data.status = 200 ) {
+            if (response.data.status = 200) {
                 if (response.data.message == 'Offre trouvée !') {
                     //console.log(response.data.offers[0].url);
                     res.send(response.data.url);
                 } else {
                     res.send('pas d\'offre correspondante');
                 }
-
             } else {
                 //console.log('no offer :(');
-                res.send('Error :'+response.data.status);
+                res.send('Error :' + response.data.status);
             }
         })
         .catch(function (error) {
