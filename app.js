bdd13ca13d41f389d144ce762516bfaecf4ab511
Chaïb Martinez <chaib@zyyne.com>
2021-03-04 13:41:08 +0100

    pep2pe

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var geturlRouter = require('./routes/geturl');
+var pep2peRouter = require('./routes/pep2pe');
 
 var app = express();
 
@@ -23,6 +24,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/geturl', geturlRouter);
+app.use('/pep2pe', pep2peRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
