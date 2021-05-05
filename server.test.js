ce3b6319004607262653c17b6ee5007b92d236e3
Chaïb Martinez <chaib@zyyne.com>
2021-05-05 16:39:39 +0200

    add some test

--- server.test.js
+++ server.test.js
@@ -1,14 +1,32 @@
 const request = require('supertest');
 const app = require('./app'); 
 
+describe('GET /',  function () {
+    it('test du chargement de la racine', function (done) {
+        request(app)
+            .get('/')
+            .expect(200, done)
+            
+    });
+});
 
-describe('GET /pep2pe', function () {
+describe('GET /pep2pe',  function () {
     it('conversion d\'un fichier d\'export talentsoft(csv) en fichier csv pole emploi', function (done) {
         request(app)
-            .get('/pep2pe?file=export_offres_PE_10-mars-2021.csv')
+            .get('/pep2pe')
+            .expect(200, done)
+            
+    });
+});
+
+describe('GET /sftp',  function () {
+    it('récupération d\'un fichier d\'export talentsoft(csv) sur leur FTP.', function (done) {
+        request(app)
+            .get('/sftp')
             .expect(200, done);
     });
 });
+
 
 
 
